2a059efc3b56239bb5ac6d4fd62b0a8d6fb772bd
vananh0110 <phamvananh1102002@gmail.com>
2024-01-23 23:07:23 +0700

    log file

---
+++ logger.h
@@ -0,0 +1,6 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+void writeToLog(const char *message);
+
+#endif
--- menu.c
+++ menu.c
@@ -34,7 +34,7 @@ void chatFunction(){
 	printf("9. Un Friend\n");
 	printf("10. Unread Message\n");
 	printf("11. Send Offline Message\n");
-	printf("Your choice (1-11):\n");
+	printf("Your choice (0-11):\n");
 	//printf("a");
 }
 
